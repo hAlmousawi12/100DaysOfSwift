6c23b9a09a66ff7416897a4a692f90db4b5c7cfe
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-05 00:41:29 +0300

    Day 1 Done

---
+++ Day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var age = 16
+var population = 8_000_000
+
+var str1 = """
+this goes
+over multiple
+lines
+"""
+
+var str2 = """
+this goes
+over multiple \
+lines
+"""
+
+var pi = 3.1415
+var awesome = true
+
+var score = 85
+var str3 = "your score was \(score)"
+var results = "The results was \(str3)"
+
+let taylor = "swift"
+
+let str4 = "Hello, Playground!"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.80
+let taylorRocks: Bool = true
+
